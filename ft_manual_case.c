84f92eeb99d82674f627270234f234c549256c1c
Dylan <kulkajens82@gmail.com>
2022-06-28 21:49:10 +0200

    Getting

--- ft_manual_case.c
+++ ft_manual_case.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hello.c                                            :+:      :+:    :+:   */
+/*   ft_manual_case.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Dylan    <kulkajens82@gmail.com>           +#+  +:+       +#+        */
+/*   By: dylan </var/spool/mail/dylan>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/25 00:36:15 by Dylan             #+#    #+#             */
-/*   Updated: 2019/11/25 00:54:49 by Dylan            ###   ########.me       */
+/*   Created: 2022/06/28 17:01:01 by dylan             #+#    #+#             */
+/*   Updated: 2022/06/28 17:11:04 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h> 
-#include <math.h>
-#include <stdlib.h>
 
-double	ft_c_to_f(double C)
-{	
-	float	one_eighth;
-	float	output_f;
+#include <stdio.h>
 
-	one_eighth = 1.8;
-	output_f = (C * one_eighth) + 32;
-	printf("%0.01f \n", output_f);
-	return (output_f);
+double	ft_manual_case(int choice)
+{
+	if (choice == 0)
+	{
+		printf("Hi \n");
+	}
+	return (0);
 }
--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hello.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dylan <kulkajens82@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2000/01/01 00:00:00 by Dylan             #+#    #+#             */
-/*   Updated: 2000/01/01 00:00:00 by Dylan            ###   ########.me       */
+/*   Updated: 2022/06/28 17:11:14 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
 double	ft_c_to_f(double C);
+double	ft_manual_case(int choice);
 
 int	main(void)
 {	
 	double	c;
+	double	choice;
+
+	scanf("%lf", &choice);
 	scanf("%lf", &c);
+	ft_manual_case(choice);
 	ft_c_to_f(c);
 	return (0);
 }
