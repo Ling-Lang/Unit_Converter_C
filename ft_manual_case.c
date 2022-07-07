2d1b8041a0fe132f7358e0b85a814732a0a79eaa
Dylan <kulkajens82@gmial.com>
2022-07-07 10:56:00 +0200

    Implementing F to C

--- ft_manual_case.c
+++ ft_manual_case.c
@@ -3,18 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_manual_case.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dylan </var/spool/mail/dylan>              +#+  +:+       +#+        */
+/*   By: dylan_u <dylan_u@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 17:01:01 by dylan             #+#    #+#             */
-/*   Updated: 2022/07/07 10:01:57 by dylan_u          ###   ########.fr       */
+/*   Updated: 2022/07/07 10:47:08 by dylan_u          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
 double	ft_c_to_f(double C);
+double	ft_f_to_c(double F);
 int		outputc(void);
 int		inputc(void);
+int		ft_manual_case2(void);
 
 double	ft_manual_case(int choice)
 {
@@ -26,6 +28,10 @@ double	ft_manual_case(int choice)
 		scanf("%lf", &c);
 		outputc();
 		ft_c_to_f(c);
+	}
+	else if (choice == 2)
+	{
+		ft_manual_case2();
 	}
 	return (0);
 }
@@ -40,4 +46,14 @@ int	outputc(void)
 {
 	printf("are this much in F \n");
 	return (0);
+}
+
+int	ft_manual_case2(void)
+{
+	double	f;
+
+	printf("Input your F wich will be converted \n");
+	scanf("%lf", &f);
+	printf("This are this much degrees in C \n");
+	ft_f_to_c(f);
 }
--- temperature.c
+++ temperature.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hello.c                                            :+:      :+:    :+:   */
+/*   temperature.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Dylan    <kulkajens82@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 00:36:15 by Dylan             #+#    #+#             */
-/*   Updated: 2019/11/25 00:54:49 by Dylan            ###   ########.me       */
+/*   Updated: 2022/07/07 10:38:33 by dylan_u          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h> 
@@ -22,4 +22,13 @@ double	ft_c_to_f(double C)
 	output_f = (C * one_eighth) + 32;
 	printf("%0.01f \n", output_f);
 	return (output_f);
+}
+
+double	ft_f_to_c(double F)
+{
+	float	output_c;
+
+	output_c = ((F - 32) / 1.8);
+	printf("%0.01f \n", output_c);
+	return (output_c);
 }
