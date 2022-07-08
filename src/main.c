13ce324ed67c22e02f6c38ec1c49988eb001ebcd
Ling-Lang <kulkajens82@gmial.com>
2022-07-08 08:20:29 +0200

    Testing Something

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dylan <kulkajens82@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2000/01/01 00:00:00 by Dylan             #+#    #+#             */
-/*   Updated: 2022/07/07 11:00:20 by dylan_u          ###   ########.fr       */
+/*   Updated: 2022/07/08 08:20:16 by dylan_u          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -16,7 +16,11 @@ int		ft_manual_case(int choice);
 int	main(void)
 {	
 	double	choice;
-
+/*
+ *
+ * Hello There Little One
+ *
+ * */
 	printf("Choose what to do. \n");
 	printf("1) Temperature Converter \n");
 	printf("2) Temperature Converter to C \n");
